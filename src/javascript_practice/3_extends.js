c68f4b43f2d8b27dce44fa319d311bad57b91e42
Emory Brunner <emorybrunner@gmail.com>
2021-06-21 20:20:52 -0400

    finished extends.js in javascript_practice

--- src/javascript_practice/3_extends.js
+++ src/javascript_practice/3_extends.js
@@ -1,2 +1,39 @@
 //*EXTENDS
+class User {
+    constructor(name) {
+        this.name = name;
+        this.type = 'Trial User'
+    };
+
+    greet() {
+        console.log(`Welcome back ${this.name}`);
+    };
+
+    status() {
+        console.log(`Current status: ${this.type}`);
+    };
+};
+
+//initial class uses default type of "Trial User" -- but what if we wanted a different type? and what if we wanted different methods for it? if we want to keep some similar properties between the classes, we can use extends.
+//the child class "extends" from the parent class. a child class may also be known as a subclass.
+
+class TrialUser extends User {
+    trialEnding() {
+        console.log(`Your trial will be ending soon, ${this.name}. Would you like to join our program?`);
+    };
+};
+
+//TrialUser is a child or subclass of User
+//therefore, TrialUser has access to the name property, for example, but User as a whole doesn't have access to the trialEnding method.
+
+//Instance of User class
+let anonDude = new User('Anonymous');
+anonDude.greet();
+anonDude.status();
+
+//Instance of TrialUser class
+let trialUser = new TrialUser('Paul');
+trialUser.greet();
+trialUser.trialEnding();
+trialUser.status();
 
